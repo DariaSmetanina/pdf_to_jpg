c46349829e3e4c312179317947331902d7e2890c
Daria <52445210+DariaSmetanina@users.noreply.github.com>
2020-07-06 15:07:42 +0300

    Update FileUploadServlet.java

--- src/main/java/FileUploadServlet.java
+++ src/main/java/FileUploadServlet.java
@@ -20,10 +20,8 @@ public class FileUploadServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        //get the file chosen by the user
         Part filePart = request.getPart("fileToUpload");
 
-        //get the InputStream to store the file somewhere
         InputStream fileInputStream = filePart.getInputStream();
 
         String filename=filePart.getSubmittedFileName();
@@ -33,15 +31,12 @@ public class FileUploadServlet extends HttpServlet {
             response.getOutputStream().println("<p>NOT PDF</p>");
             return;
         }
-        //for example, you can copy the uploaded file to the server
-        //note that you probably don't want to do this in real life!
-        //upload it to a file host like S3 or GCS instead
+ 
         filename=decodeGetParameter(filename.substring(0,filename.length()-4))+".pdf";
         File fileToSave = new File("C:/Users/Dasha/Desktop/res/" + filename);
         Files.copy(fileInputStream, fileToSave.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
 
-        //create output HTML that uses the
         response.getOutputStream().println("<p>File uploaded</p>");
 
         Convert.ConvertPDF(filename);
